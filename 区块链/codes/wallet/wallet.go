9edffb0db07039ed54fca176450bb8df5485ae2a
jeekhell@gmail.com <jeekhell@gmail.com>
2022-01-02 16:49:09 +0400

    update

--- 区块链/codes/wallet/wallet.go
+++ 区块链/codes/wallet/wallet.go
@@ -4,5 +4,13 @@ import "crypto"
 
 type Wallet struct {
 	// 1、私钥
-	crypto.PrivateKey
+	PrivateKey crypto.PrivateKey
+	// 2、公钥
+	PublicKey  []byte
+}
+
+func NewWallet() *Wallet {
+	return Wallet{
+		PrivateKey: 
+	}
 }
